e2058be5e3aeebae01f8365ad11d5ea624c3e443
Tatjana Zekic <tatjana.zekic93@gmail.com>
2019-10-04 09:48:09 +0200

    Initial commit

---
+++ ProjectForGitTasks/NewFolder1/LoginPage.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForGitTasks.NewFolder1
+{
+    class LoginPage
+    {
+
+
+        IWebDriver driver;
+        By username = By.Id("txtUsername");
+        By passsword = By.Id("txtPassword");
+        By loginButton = By.Id("btnLogin");
+        By forgotYourPasswordLink = By.XPath("//*[@id='forgotPasswordLink']/a");
+        By usernameLoginMessage = By.Id("spanMessage");
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void TypeUsername(string userName)
+        {
+            driver.FindElement(username).Clear();
+            driver.FindElement(username).SendKeys(userName);
+        }
+        public void TypePassword(string passWord)
+        {
+            driver.FindElement(passsword).Clear();
+            driver.FindElement(passsword).SendKeys(passWord);
+
+        }
+
+        public void ClickOnLoginButton()
+        {
+            driver.FindElement(loginButton).Click();
+        }
+
+        public void ClickOnForgotYourPassword()
+        {
+            driver.FindElement(forgotYourPasswordLink).Click();
+
+        }
+        public string GetLoginWarningMessage()
+        {
+            var element = driver.FindElement(usernameLoginMessage);
+            return element.Text;
+        }
+
+
+    }
+}
