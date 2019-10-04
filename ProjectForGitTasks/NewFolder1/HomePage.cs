92e9dec2e7f68b9cfd1bc407b4dc9bd2c6e3d4b4
Tatjana Zekic <tatjana.zekic93@gmail.com>
2019-10-04 10:19:49 +0200

    added sth in HomePage class

--- ProjectForGitTasks/NewFolder1/HomePage.cs
+++ ProjectForGitTasks/NewFolder1/HomePage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace ProjectForGitTasks.NewFolder1
 {
     class HomePage
     {
+        IWebDriver driver;
+
+        public HomePage()
+        {
+            this.driver = driver;
+        }
     }
 }
